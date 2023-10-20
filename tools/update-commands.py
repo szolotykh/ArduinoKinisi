fa9d9e2bb66c84636b3ed62959bef9e32a79f3f9
Sergey Zolotykh <szolotykh88@gmail.com>
2023-10-20 00:01:46 -0700

    Update update-commands script

--- tools/update-commands.py
+++ tools/update-commands.py
@@ -7,8 +7,6 @@ branch = 'main'
 
 files = [f'https://raw.githubusercontent.com/szolotykh/kinisi-motor-controller-firmware/{branch}/commands.json']
 
-shutil.rmtree('./', ignore_errors=True)
-os.mkdir('./')
 for url in files:
   response = requests.get(url)
   with open(f"./{url.split('/')[-1]}", 'wb') as f:
